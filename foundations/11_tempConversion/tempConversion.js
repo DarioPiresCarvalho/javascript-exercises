44c079df30a067da7c21f269eb1f0016f95b600c
DarioPiresCarvalho <dariopires99@outlook.com>
2026-08-17 14:43:15 +0100

    solving 11 without the multiplying and dividing by 10, rather using toFixed() to specify decimal values

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsius = ((temperature - 32)*(5/9))
+  if (celsius === 0 || Number.isInteger(celsius) ) {
+    return Number(celsius)
+  } else {
+    return Number(celsius.toFixed(1))
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = (temperature * (9/5)) + 32
+  if (fahrenheit === 0 || Number.isInteger(fahrenheit)) {
+    return Number(fahrenheit)
+  } else {
+    return Number(fahrenheit.toFixed(1))
+  }
 };
 
 // Do not edit below this line
