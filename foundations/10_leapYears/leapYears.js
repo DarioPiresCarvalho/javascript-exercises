98ea72f8ce63aaffdb2cb3e13c6ba18844b44094
DarioPiresCarvalho <dariopires99@outlook.com>
2026-08-17 14:16:17 +0100

    Solution for 10, if statement could be made more compact

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(value) {
+
+    let leap = false
+
+    if (value%4 == 0) {
+        if (value%100 == 0 && value%400 == 0) {
+            leap = true
+        } else if (value % 100 != 0) {
+            leap = true
+        }
+        return leap
+    } else {
+        return leap
+    }
+    
 
 };
 
